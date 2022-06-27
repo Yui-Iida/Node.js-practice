dd9d87dab4da4476b9fdf49c91aeca86e2bac7f7
Yui-Iida <yxuxi328@icloud.com>
2022-06-27 17:05:22 +0200

    first commit

--- src/index.js
+++ src/index.js
@@ -1,2 +1,5 @@
-export { default as Greeting }  from './hello.js';
+export { default as Greeting } from './hello.js';
 
+// import Greeting from './hello.js';
+// const greeting = new Greeting();
+// alert(greeting.sayHello());
